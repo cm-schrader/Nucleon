10bf32052e5946667d71ffb355e3051856e93f9e
Christian M. Schrader <cmschrader@wpi.edu>
2020-07-04 22:19:13 -0400

    SPI added

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,12 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define SPI1_CS_Pin GPIO_PIN_4
+#define SPI1_CS_GPIO_Port GPIOA
+#define SPI1_SDI_Pin GPIO_PIN_6
+#define SPI1_SDI_GPIO_Port GPIOA
+#define SPI1_SDO_Pin GPIO_PIN_7
+#define SPI1_SDO_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
