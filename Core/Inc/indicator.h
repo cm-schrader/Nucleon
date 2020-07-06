46e449622d82f657b85abb6da8517f17cfc84373
Christian M. Schrader <cmschrader@wpi.edu>
2020-07-06 17:08:37 -0400

    Indicator Added

---
+++ Core/Inc/indicator.h
@@ -0,0 +1,21 @@
+/**
+  ******************************************************************************
+  * @file           : indicator.c
+  * @brief          : Buzzer and Status LED management
+  * @date			: July 6th, 2020
+  *
+  * 	Interfaces with the Status LED and Piezo buzzer.
+  ******************************************************************************
+  */
+
+
+
+#ifndef INC_INDICATOR_H_
+#define INC_INDICATOR_H_
+
+/*  API Functions */
+
+/*  Hardware Functions */
+
+
+#endif /* INC_INDICATOR_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -293,7 +293,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 440;
+  sConfigOC.Pulse = 500;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
